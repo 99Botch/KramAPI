f5a8be25470786a39de77df29ffe4677dfa0d603
99Botch <yvanmonod@protonmail.com>
2022-02-11 13:17:39 +0100

    Updayed the update user controller & added comments

--- routes/users.router.js
+++ routes/users.router.js
@@ -60,7 +60,7 @@ router.post('/login', async (req, res) => {
     if(!validPassword) return res.status(400).json({ message: 'Invalid password' });   
 
     // tokens are made of: 1. User _id 2. user mail 3. Secret key (.env) and will expire after 3 hours
-    const token = jwt.sign( { _id: user.id, email: user.email }, process.env.SECRET_KEY, { expiresIn: '3h' });
+    const token = jwt.sign( { _id: user.id }, process.env.SECRET_KEY, { expiresIn: '3h' });
     res.json(token);
 });
 
@@ -93,89 +93,64 @@ router.get('/:id', async (req, res) => {
 router.put('/:id', verify, async (req, res) => {
     // refer to 'verify' in ../config/tokenValidation
     try{
-        // first step consists to check the validity of the token by comparing the _id from the token against the id of the session
-        // req.params property is an object containing properties mapped to the named route “parameters” (e.g., the id passed in the URI)
+        // first step consists to ccheck whether the user tries to update his profile or not
         let id = req.params.id || {};
         if (id != req.user._id) return res.status(401).json("Ids aren't matching");
 
-        // for updateValidation refer to ../config/validation -> if true, the API renders the error
+        // for updateValidation refer to ../config/validation -> if the format attributes format aren't respected, API throws an error
         const { error } = await updateValidation(req.body);
         if(error) return res.status(400).json(error.details[0]);
 
-        // isCreditsTaken checks the uniqueness of the username & mail address via findOne which sends a query to the db -> if true, the API renders  the error
-        const user =  await User.findById(id);
-        let username =  user.username;
-        let email =  user.email;
-
-        if(!user) return res.status(404).json("user not found");
-
-        const isCreditsTaken = await User.findOne({ 
-            $or: [{ username: req.body.username }, { email: req.body.email }]
-        });
-
-        if (!isCreditsTaken) return res.status(200).json('success')
-
-
-        if (id !== isCreditsTaken._id && (username != req.body.username && email !=  req.body.email)) return res.status(400).json('MEEEEEEEERDE');
-        return res.status(200).json('Username or Email is already taken ');
-
-        /*if (isCreditsTaken){
-            console.log(username)
-            console.log(req.body.username)
-            // if botch old mail & old username
-            if (username == req.body.username && email == req.body.email){
-                return res.status(200).json('success 1');
-            }
-
-            if ((email != req.body.email && username == req.body.username) && req.body.email !== email ){
-                return res.status(400).json('Username or Email is already taken 2');
-            }
-
-            if ((username != req.body.username && username == req.body.email) && req.body.username !== username ){
-                return res.status(400).json('Username or Email is already taken 3');
-            }
-
-            // if old username and new mail
-            if (username == req.body.username && email != req.body.email){
-                return res.status(200).json('success 2');
-            }
-
-            // if old mail nd anew username
-            if (email == req.body.email && username != req.body.username){
-                return res.status(200).json('success 3');
-            }
-
-
-            else{
-                return res.status(400).json('Username or Email is already taken 1');
-            }
-
-        }*/
-
+        // Promise.all sends to request with the promise to return both queries or none
+        // the first query gets the user details while the second checks whether the update details already exists
+        Promise.all([
+            User.findOne({ _id: id }),
+            User.findOne({ 
+                $or: [{ username: req.body.username }, { email: req.body.email }] 
+            })
+        ])
+        .then( async ([ user, isCreditsTaken ]) => {
+            // user details
+            let old_username = user.username, old_email = user.email
     
+            // create new object to push to the query
+            const updUser = await new User({
+                _id: id,
+                username: req.body.username,
+                email: req.body.email,
+                profile_pic_url: req.body.profile_pic_url
+            });
+            
+            // User is pushed to the database if the details don't exists or if the user is not updating is mail/username
+            if (!isCreditsTaken || isCreditsTaken && old_username == req.body.username && old_email == req.body.email){
+                let updateValidation = await updateQuery(updUser);
+                return res.status(200).json(updateValidation);
+            }
 
-        
-        
-        return res.status(400).json('Username or Email is already taken ');
-        // _katyusha
-        // _alpha
+            // if either req has old mail and anew username or if req has old username and new mail
+            if (isCreditsTaken && (old_username == req.body.username && old_email != req.body.email) || (old_email == req.body.email && old_username != req.body.username)){
+                let updateValidation = await updateQuery(updUser);
+                return res.status(200).json(updateValidation);
+            }
 
-        // user.username 
-        // user.email
-
-        // updateUser is a new object of the user schema which may take the profile_pic_url attribute. _id act as the identifier for the db
-        // const updateUser = await User.updateOne(
-        //     {_id: req.params.id}, 
-        //     { $set: { 
-        //         username: req.body.username, 
-        //         email: req.body.email,
-        //         profile_pic_url: req.body.profile_pic_url 
-        //     }}
-        // );
-        // res.json(updateUser);
+            return res.status(400).json('Username or Email is already taken');
+        });
 
     } catch(err) { res.json({message: err}) }
 });  
+
+async function updateQuery (_updUser) {
+    // updateUser is a new object of the user schema which may take the profile_pic_url attribute. _id act as the identifier for the db
+    const updateUser = await User.updateOne(
+        {_id: _updUser._id}, 
+        { $set: { 
+            username: _updUser.username, 
+            email: _updUser.email,
+            profile_pic_url: _updUser.profile_pic_url 
+        }}
+    );
+    return updateUser;
+}
 
 
 // DELETE USER PROFILE
